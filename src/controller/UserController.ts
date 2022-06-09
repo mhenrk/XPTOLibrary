d5cfa2e7dc5420526df19ae212d2fb7ca387091c
Marcio H <marcio.o@github.com>
2022-06-09 09:40:14 -0300

    updated files

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,41 +1,39 @@
+import User from '@src/type/User';
 import AbstractController from './AbstractController';
-import models from '../models';
 
 class UserController extends AbstractController {
     // TODO: Add your code here
-    public async createUser(req: Request, res: Response): Promise<Object> {
-        const user = req.body;
-        const newUser = models.UserRoute.create(user);
+    public async createUser(req: Request, res: Response): Promise<User> {
+        const user = req.body;        
 
-        return newUser
     }
 
-    public async listUser(req: Request, res: Response): Promise<void> {
-        const { userId } = req.query;
-        if (!userId) {
-            //** Listar apenas o usuario pesquisado */
-            //** Retornar o usuário */
-        }
+    // public async listUser(req: Request, res: Response): Promise<void> {
+    //     const { userId } = req.query;
+    //     if (!userId) {
+    //         //** Listar apenas o usuario pesquisado */
+    //         //** Retornar o usuário */
+    //     }
 
-        /**
-         * Exibo todos os usuários aqui
-         * Retornar a lista de todos usuários
-         */
-    }
+    //     /**
+    //      * Exibo todos os usuários aqui
+    //      * Retornar a lista de todos usuários
+    //      */
+    // }
 
-    public async handleUser(req: Request, res: Response): Promise<void> {
-        const { id } = req.params;
-        const { activated } = req.query;
+    // public async handleUser(req: Request, res: Response): Promise<void> {
+    //     const { id } = req.params;
+    //     const { activated } = req.query;
 
-        if (!id) {
-            res.status(404).json({ message: "id not found" });
-        }
+    //     if (!id) {
+    //         res.status(404).json({ message: "id not found" });
+    //     }
 
-        /**
-         * database user searching
-         * set user attribute activated (true / false - "toggle button")
-         */
-    }
+    //     /**
+    //      * database user searching
+    //      * set user attribute activated (true / false - "toggle button")
+    //      */
+    // }
 }
 
 export default UserController;
--- src/route/UserRoute.ts
+++ src/route/UserRoute.ts
@@ -1,4 +1,6 @@
 import UserController from '@src/controller/UserController';
+import HttpMethod from '@src/enum/HttpMethod';
+import UserModel from '@src/models/user';
 import RouteMethod from '@src/type/RouteMethod';
 import User from '@src/type/User';
 import AbstractRoute from './AbstractRoute';
@@ -6,6 +8,12 @@ import AbstractRoute from './AbstractRoute';
 class UserRoute extends AbstractRoute<UserController, User> {
     protected allowedRouteMethods: RouteMethod<UserController>[] = [
         // TODO: Add your code here
+        {
+            "httpMethod": HttpMethod.POST,
+            "methodName": "createUser",
+            "path": "/api/user",
+            "standardCode": 200
+        },
     ];
     
 }
